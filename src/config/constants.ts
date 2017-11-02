3658d10f017bd35fd72fa4ad5e8cd145de0ad89b
Alex Whitaker <whitaker.alexander@gmail.com>
2017-11-02 05:57:27 -0500

    add constants file

---
+++ src/config/constants.ts
@@ -0,0 +1,13 @@
+export const constants = {
+  initialMessage: '{greeting} {firstName}, and welcome to {company}! Room {roomNumber} is now ready you. ' +
+  'Enjoy your stay, and let us know if you need anything.',
+};
+
+export const formErrors = {
+  selectGuest: 'Please Select a Guest',
+  selectCompany: 'Please Select a Company'
+}
+
+export const defaultPlaceholders = {
+  greeting: 'greeting'
+}
