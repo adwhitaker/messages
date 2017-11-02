a2a3dd0f30f8ee27ff7f5fdb42177cfc33989b38
Alex Whitaker <whitaker.alexander@gmail.com>
2017-11-02 11:52:57 -0500

    validate new template name

--- src/app/add-template-form/add-template-form.component.ts
+++ src/app/add-template-form/add-template-form.component.ts
@@ -8,15 +8,24 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class AddTemplateFormComponent {
   @Output() private addMessageTemplate: EventEmitter<string>;
   public name: string;
+  public error: boolean;
 
   constructor() {
     this.addMessageTemplate = new EventEmitter();
+    this.name = '';
+    this.error = false
   }
 
   public addTemplate(): void {
     console.assert(this.addMessageTemplate != null, 'Assertion Fail @ AddTemplateFormComponent#addTemplate: No addMessageTemplate');
     console.assert(this.addMessageTemplate.emit != null, 'Assertion Fail @ AddTemplateFormComponent#addTemplate: No emit');
 
+    if (!this.name) {
+      this.error = true;
+      return;
+    }
+
+    this.error = false;
     this.addMessageTemplate.emit(this.name);
     this.name = '';
   }
